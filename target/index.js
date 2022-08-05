36bbbc0819ba54b6e4c71cd6c2d8d604cd1c029e
Acier <65429526+AcierDev@users.noreply.github.com>
2022-08-05 11:28:54 -0400

    Delete target directory

--- target/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ClientWrapper_1 = require("./ClientWrapper");
-require('dotenv').config();
-new ClientWrapper_1.ClientWrapper();
-ClientWrapper_1.ClientWrapper.get().login(process.env.TOKEN);
